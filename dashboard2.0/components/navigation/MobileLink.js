d760f4eaa938123479016972a6759cbac0b79588
talha1725 <talhanoor1001@gmail.com>
2024-08-19 23:15:54 +0500

    replicated the code in next js application

--- dashboard2.0/components/navigation/MobileLink.js
+++ dashboard2.0/components/navigation/MobileLink.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Link } from "react-router-dom"
+import Link from 'next/link'
 import IconPlacer from "../common/IconPlacer"
 
 function MobileLink({ link, handleToggle }) {
