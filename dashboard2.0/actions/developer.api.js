043740920e3eda70b4c213f0386d4abeac5419ac
Hasan <51920911+Hasanraza786@users.noreply.github.com>
2024-08-23 18:25:51 +0500

    Update location of existing User

--- dashboard2.0/actions/developer.api.js
+++ dashboard2.0/actions/developer.api.js
@@ -1,26 +1,26 @@
 "use server";
 
-import {developers} from "@/lib/db/schema";
-import {db} from "@/lib/db/connect";
-import {eq} from "drizzle-orm";
+import { developers } from "@/lib/db/schema";
+import { db } from "@/lib/db/connect";
+import { eq } from "drizzle-orm";
 
 export async function addDeveloper(user) {
 	try {
 		// Ensure that top_languages is an array
 		const topLanguagesArray = Array.isArray(user.githubDetails.top_languages) ? user.githubDetails.top_languages : [];
-		
+
 		// Convert the skills string back to an array
 		const skillsArray = user.githubDetails.top_languages.join(', ').split(', ');
-		
+
 		console.log("Generated skills array:", skillsArray);
-		
+
 		// 1. Check if the user exists in the database using their GitHub ID
 		const existingUser = await db.query.developers.findFirst({
-			where: (developer, {eq}) => eq(developers.githubUsername, user.githubDetails.login)
+			where: (developer, { eq }) => eq(developers.githubUsername, user.githubDetails.login)
 		});
-		
-		console.log("Existing user:", existingUser);
-		
+
+		console.log("Existing user Name Details:", existingUser?.name , existingUser?.githubUsername);
+
 		if (!existingUser) {
 			// Case 1: User does not exist in the database, so add the user
 			console.log("User not found, inserting new user");
@@ -36,17 +36,17 @@ export async function addDeveloper(user) {
 				location: user.githubDetails.location,
 				publicRepositories: user.githubDetails.public_repos,
 			});
-			
+
 			console.log("Developer added successfully: ", developer);
 			return true;
 		}
-		
+
 		// 2. Check if the data has changed
 		const dataChanged = (existingUser.name !== user.name || existingUser.email !== user.email || existingUser.skills.join(', ') !== skillsArray.join(', ') ||  // Compare skills as strings
-			existingUser.publicRepositories !== user.githubDetails.public_repos || existingUser.imageUrl !== user.image);
-		
+			existingUser.publicRepositories !== user.githubDetails.public_repos || existingUser.imageUrl !== user.image || existingUser?.location !== user?.githubDetails?.location);
+
 		console.log("Data changed:", dataChanged);
-		
+
 		if (dataChanged) {
 			// Case 2: User exists, but data has changed, so update the user
 			console.log("User data has changed, updating user");
@@ -54,19 +54,21 @@ export async function addDeveloper(user) {
 				.set({
 					name: user.name, email: user.email, topLanguages: topLanguagesArray,  // Update topLanguages array
 					skills: skillsArray,  // Update the skills array
-					publicRepositories: user.githubDetails.public_repos, imageUrl: user.image,  // Update the image URL if it has changed
+					publicRepositories: user.githubDetails.public_repos, imageUrl: user.image,
+					location: user?.githubDetails?.location
+					// Update the image URL if it has changed
 				})
 				.where(eq(developers.githubUsername, user.githubDetails.login))
 				.execute();
-			
+
 			console.log("User updated successfully");
 			return true;
 		}
-		
+
 		// Case 3: User exists and data has not changed, do nothing
 		console.log("User exists and no data changes detected, nothing to do");
 		return true;
-		
+
 	} catch (error) {
 		console.error('Error processing user:', error);
 		return null;
@@ -79,9 +81,9 @@ export async function getDeveloper(developerID) {
 	try {
 		const developer = await db.select().from(developers).where(eq(developers.id, Number(developerID)));
 		console.log('developer:', developer)
-		
+
 		return developer[0]
-		
+
 	} catch (error) {
 		console.error('Error processing user:', error);
 		return null;
@@ -141,16 +143,16 @@ export async function ChangeDeveloperPassword(passwordData, developerId) {
 			where: (developer) => eq(developer.id, developerId),
 			columns: ['password']  // Make sure 'password' is a field in your schema
 		});
-		
+
 		if (!developer) {
 			throw new Error('Developer not found');
 		}
-		
+
 		// 2. Compare the provided current password with the stored password
 		if (passwordData.current_password !== developer.password) {
 			throw new Error('Current password is incorrect');
 		}
-		
+
 		// 3. Update the developer's password in the database
 		await db.update(developers)
 			.set({
@@ -158,7 +160,7 @@ export async function ChangeDeveloperPassword(passwordData, developerId) {
 			})
 			.where(eq(developers.id, developerId))
 			.execute();
-		
+
 		console.log('Password updated successfully');
 		return true;
 	} catch (error) {
