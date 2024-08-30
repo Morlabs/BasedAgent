59129920a0c9c866f93dea6cce03e039efddeb34
Talha1725 <talhanoor1001@gmail.com>
2024-08-30 09:27:25 +0500

    addded credentials for auth

--- dashboard2.0/app/api/auth/[...nextauth]/route.js
+++ dashboard2.0/app/api/auth/[...nextauth]/route.js
@@ -3,19 +3,19 @@ import NextAuth from "next-auth"
 
 // importing providers
 import GithubProvider from "next-auth/providers/github"
-import { getTopLanguages, getTotalContributions, getUserDetails, getExtra } from '@/helpers/github'
-import { calculateDeveloperWeight } from '@/actions/calculateDeveloperWeight.api';
-import { validateReferralSignIn } from "@/actions/validateReferralSignIn.api";
+import {getTopLanguages, getTotalContributions, getUserDetails, getExtra} from '@/helpers/github'
+import {calculateDeveloperWeight} from '@/actions/calculateDeveloperWeight.api';
+import {validateReferralSignIn} from "@/actions/validateReferralSignIn.api";
 
 const handler = NextAuth({
 	providers: [
 		GithubProvider({
-			clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
-			clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET,
+			clientId: 'Ov23liqqaWOhYy8noV6M',
+			clientSecret: '36c2c5f34ed4649a23d195135dd7ce7838c8100c',
 		})
 	],
 	callbacks: {
-		async session({ session, token, user }) {
+		async session({session, token, user}) {
 			// Safely add user details to the session object
 			session.user.id = token.sub ?? null;
 			session.user.accessToken = token?.accessToken ?? null;
@@ -23,19 +23,20 @@ const handler = NextAuth({
 			session.user.githubDetails.top_languages = await getTopLanguages(token?.accessToken, session.user.githubDetails.login);
 			session.user.githubDetails.total_contribution = await getTotalContributions(token?.accessToken, session.user.githubDetails.login);
 			session.user.githubDetails.extra = await getExtra(token?.accessToken, session.user.githubDetails.login);
-			session.user.weight = await calculateDeveloperWeight(session.user);
-
+			// session.user.weight = await calculateDeveloperWeight(session.user);
+			session.user.weight = {};
+			
 			try {
 				let isReferral = await validateReferralSignIn(session.user.email);
 				console.log('isReferral:', isReferral);
 			} catch (error) {
 				console.log('Error in validateReferralSignIn:', error.message);
-				return Response.json({ error: error.message }, { status: 500 });
+				return Response.json({error: error.message}, {status: 500});
 			}
-
+			
 			return session;
 		},
-		async jwt({ token, user, account, profile }) {
+		async jwt({token, user, account, profile}) {
 			// Persist the OAuth access token and the user id to the token right after signin
 			if (account) {
 				token.accessToken = account.access_token;
@@ -48,4 +49,4 @@ const handler = NextAuth({
 	}
 });
 
-export { handler as GET, handler as POST };
+export {handler as GET, handler as POST};
--- dashboard2.0/app/user/page.js
+++ dashboard2.0/app/user/page.js
@@ -1,39 +1,39 @@
 'use client'
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { useAuth } from "@/hooks/useAuth";
+import React, {useEffect} from 'react';
+import {useRouter} from 'next/navigation';
+import {useAuth} from "@/hooks/useAuth";
 import LoaderLocal from "@/components/common/loaderLocal";
-import { addDeveloper } from "@/actions/developer.api";
+import {addDeveloper} from "@/actions/developer.api";
 // Assuming this is the API you want to call
 
 const TempRedirect = () => {
 	const router = useRouter();
-	const { isLoggedIn, user, isLoading } = useAuth();
-
+	const {isLoggedIn, user, isLoading} = useAuth();
+	console.log('user from user page', user);
 	useEffect(() => {
 		const handleRedirect = async () => {
 			if (isLoggedIn && user?.id) {
 				// Example API call
 				try {
 					const response = await addDeveloper(user);
-
+					
 					console.log('API response:', response); // Handle the response
 				} catch (error) {
 					console.error('API error:', error); // Handle any errors
 				}
-
+				
 				router.push(`/user/${user.id}`);
 			} else if (!isLoading && !isLoggedIn) {
 				router.push('/');
 			}
 		};
-
+		
 		handleRedirect(); // Call the async function inside useEffect
 	}, [isLoggedIn, user, isLoading, router]);
-
+	
 	return (
 		<div className="flex flex-col justify-center items-center h-screen">
-			<LoaderLocal />
+			<LoaderLocal/>
 			<div className="mt-4 text-center">Authenticating</div>
 		</div>
 	);
